8fcea3a44d4bc4a457fa88ad4941ddd057514bb8
Nicolas Sirghii <nicolas.mailbox100@gmail.com>
2026-02-23 20:13:12 +0200

    added backend

--- Ecommerce/src/App.jsx
+++ Ecommerce/src/App.jsx
@@ -4,14 +4,22 @@ import { Routes, Route } from "react-router"
 import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/CheckoutPage'
 import { OrdersPage } from './pages/OrdersPage'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
   
+ const [cart, setCart] = useState([])
 
+ useEffect(() => {
+    axios.get("/api/cart-items?expand=product")
+    .then((response)=>{setCart(response.data)})
+ },[])
+  
   return (
     <Routes>
-        <Route index element = {<HomePage />} />
-        <Route path='checkout' element = {<CheckoutPage />} />
+        <Route index element = {<HomePage cart={cart} />} />
+        <Route path='checkout' element = {<CheckoutPage cart={cart}/>} />
         <Route path='orders' element = {<OrdersPage />} />
     </Routes>
   
